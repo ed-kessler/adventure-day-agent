405d63fd030bd48707f8c6afaf7dd385899ab521
Mayumi Egawa <105865161+m-eg@users.noreply.github.com>
2025-04-07 09:50:40 +0000

    Fixed the initial PHASE1 request to ensure it processes correctly.

--- src-agents/phase1/main.py
+++ src-agents/phase1/main.py
@@ -57,11 +57,14 @@ async def ask_question(ask: Ask):
 
     # Send a completion call to generate an answer
     print('Sending a request to openai')
-    start_phrase =  ask.question
-    response: openai.types.chat.chat_completion.ChatCompletion = None
+    start_phrase = ask.question
+    response = client.chat.completions.create(
+        model = deployment_name,
+        messages = [{"role" : "assistant", "content" : start_phrase}],
+    )
 
-    # Send a completion call to generate an answer
-
+    print(response.choices[0].message.content)
+    print(response)
     answer = Answer(answer=response.choices[0].message.content)
     answer.correlationToken = ask.correlationToken
     answer.promptTokensUsed = response.usage.prompt_tokens
